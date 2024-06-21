bafe5766df4e09d882184235973b7a1aa2635649
Deborah Chrotich <cherotichdeborahb@gmail.com>
2024-06-21 11:41:54 +0300

    fetch users

---
+++ src/About/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+
+const About = () =>{
+    return(
+        <div className="about-text">
+            <p>We are learning about JSX</p>
+            <p>We are coding a new front-end library</p>
+        </div>
+    )
+}
+export default About;
